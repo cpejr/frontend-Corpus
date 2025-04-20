4e9647bcdb512188e8c0b33c7e5f9e061530717a
Lucas Akatsuka <ljoakatsuka@hotmail.com>
2025-04-20 19:44:01 -0300

    fix: calendar language fixed

--- src/components/common/Calendar/Calendar.jsx
+++ src/components/common/Calendar/Calendar.jsx
@@ -2,6 +2,60 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Controller } from "react-hook-form";
 import { Container, StyledCalendar } from "./Styles";
+import { useGlobalLanguage } from "../../../stores/globalLanguage";
+import { addLocale } from "primereact/api";
+
+
+addLocale('pt-BR', {
+  firstDayOfWeek: 0,
+  dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  monthNamesShort: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  today: 'Hoje',
+  clear: 'Limpar'
+});
+
+addLocale('en-US', {
+  firstDayOfWeek: 0,
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  monthNamesShort: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  today: 'Today',
+  clear: 'Clear'
+});
+
+addLocale('es-ES', {
+  firstDayOfWeek: 1,
+  dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+  dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+  monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  monthNamesShort: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  today: 'Hoy',
+  clear: 'Limpiar'
+});
+
+addLocale('de-DE', {
+  firstDayOfWeek: 1,
+  dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+  dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+  dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'], 
+  monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+  monthNamesShort: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+  today: 'Heute',
+  clear: 'Löschen'
+});
+
+const localeMap = {
+  PT: "pt-BR",
+  EN: "en-US",
+  ES: "es-ES",
+  GE: "de-DE"
+};
 
 export default function CalendarFunction({
   inputKey,
@@ -15,9 +69,11 @@ export default function CalendarFunction({
   dateFormat,
 }) {
   const [date, setDate] = useState(defaultValue || "");
+  const { globalLanguage } = useGlobalLanguage();
+  const locale = localeMap[globalLanguage] || "en-US";
 
   const handleChange = (dateChange) => {
-    setValue("birthday", dateChange.toLocaleDateString("pt-BR"), {
+    setValue("birthday", dateChange.toLocaleDateString(locale), {
       shouldDirty: true,
     });
 
@@ -48,6 +104,7 @@ export default function CalendarFunction({
             dateFormat={dateFormat}
             view="date"
             showButtonBar
+            locale={locale}
           />
         )}
       />
