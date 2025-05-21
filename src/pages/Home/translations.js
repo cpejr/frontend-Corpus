63785190d86cc23f6c124719ad1ea89d27c10f10
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2025-05-21 18:41:08 -0300

    feat: translating Home Page new texts by now

--- src/pages/Home/translations.js
+++ src/pages/Home/translations.js
@@ -31,15 +31,14 @@ export function TranslateText({ globalLanguage }) {
     subtitleLogo = "in Multimodal Interactions";
     title1 = "About us";
     paragraph1 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "The International Research Center Intercultural Communication in Multimodal Interactions (ICMI) is an interinstitutional and international network of researchers. Since 2010, our work has pioneered the investigation of conversational, interactional, translingual, multimodal, and linguistic-cognitive phenomena. Currently, our researchers have been dedicated to developing innovative methodologies for the transcription and multimodal analysis of video-mediated interactions in virtual contexts.";
     textButton1 = "learn more!";
-    title2 = "Videos";
+    title2 = "Data and Preservation";
     paragraph2 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "The data in this project, registered under (CAAE: 55218521.1.1001.5149), have been an expanding resource since 2010, documenting interactions in diverse intercultural contexts. This platform has been developed for the preservation and availability of these data, with the aim of creating a robust repository that supports detailed multimodal analyses.";
     textButton2 = "learn more!";
     title3 = "Privacy policies";
-    paragraph3 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+    paragraph3 = "Under construction";
     textButton3 = "learn more!";
   }
   if (globalLanguage === "ES") {
@@ -47,15 +46,14 @@ export function TranslateText({ globalLanguage }) {
     subtitleLogo = "en Interacciones Multimodales";
     title1 = "Sobre nosotros";
     paragraph1 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "El Centro de Investigación Internacional Intercultural Communication in Multimodal Interactions (ICMI) es una red interinstitucional e internacional de investigadores. Desde 2010, nuestro trabajo ha sido pionero en la investigación de fenómenos conversacionales, interaccionales, translingüísticos, multimodales y lingüístico-cognitivos. Actualmente, nuestros investigadores se han dedicado al desarrollo de metodologías innovadoras para la transcripción y el análisis multimodal de interacciones mediadas por video, en contextos virtuales.";
     textButton1 = "aprende más!";
-    title2 = "Administrar videos";
+    title2 = "Datos y Preservación";
     paragraph2 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "Los datos de este proyecto, registrados bajo el (CAAE: 55218521.1.1001.5149), son un recurso en expansión desde 2010, documentando interacciones en diversos contextos interculturales. Esta plataforma ha sido construida para la preservación y la disponibilidad de estos datos, con el objetivo de crear un banco robusto que respalde análisis multimodales detallados.";
     textButton2 = "aprende más!";
     title3 = "Politicas de privacidad";
-    paragraph3 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+    paragraph3 = "En construcción";
     textButton3 = "aprende más!";
   }
   if (globalLanguage === "GE") {
@@ -63,15 +61,14 @@ export function TranslateText({ globalLanguage }) {
     subtitleLogo = "in multimodalen Interaktionen";
     title1 = "Über uns";
     paragraph1 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "Das Internationale Forschungszentrum Intercultural Communication in Multimodal Interactions (ICMI) ist ein interinstitutionelles und internationales Netzwerk von Forschenden. Seit 2010 leistet unsere Arbeit Pionierarbeit in der Untersuchung von konversationellen, interaktionalen, translingualen, multimodalen und sprachlich-kognitiven Phänomenen. Derzeit widmen sich unsere Forschenden der Entwicklung innovativer Methoden zur Transkription und multimodalen Analyse von videobasierten Interaktionen in virtuellen Kontexten.";
     textButton1 = "mehr erfahren!";
-    title2 = "Videos";
+    title2 = "Daten und Erhaltung";
     paragraph2 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+      "Die in diesem Projekt enthaltenen Daten, registriert unter (CAAE: 55218521.1.1001.5149), stellen seit 2010 eine wachsende Ressource dar, die Interaktionen in verschiedenen interkulturellen Kontexten dokumentiert. Diese Plattform wurde zur Bewahrung und Bereitstellung dieser Daten aufgebaut, mit dem Ziel, eine robuste Datenbank zu schaffen, die detaillierte multimodale Analysen unterstützt.";
     textButton2 = "mehr erfahren!";
     title3 = "Datenschutzrichtlinien";
-    paragraph3 =
-      "Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam. Id fugit iusto est possimus asperiores qui repellat nesciunt. Lorem ipsum dolor sit amet. Ut dolorum consequatur ut fugiat ducimus et quia saepe et recusandae incidunt qui impedit sunt et molestiae veniam et obcaecati ipsam.";
+    paragraph3 = "Im Aufbau";
     textButton3 = "mehr erfahren!";
   }
 
