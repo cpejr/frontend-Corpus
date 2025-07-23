3fb055497e2cf49509e073ae2afd34c1b1450e6a
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2025-07-23 16:43:02 -0300

    Feat: New images added

--- src/pages/VideoPage/utils.js
+++ src/pages/VideoPage/utils.js
@@ -3,6 +3,6 @@ import { z } from "zod";
 // Form Validation
 export const validationSchema = () => {
   return z.object({
-    ManualTranscriptionArchive: z.string({ required_error: "insira um video" }),
+    //ManualTranscriptionArchive: z.string({ required_error: "insira um video" }),
   });
 };
