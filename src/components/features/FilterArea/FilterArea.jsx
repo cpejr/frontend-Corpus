8563433737bd5deb3d3455b5377f2ec9892bf48d
Aline <aline.molinar@hotmail.com>
2025-08-18 21:48:36 -0300

    fix: filters

--- src/components/features/FilterArea/FilterArea.jsx
+++ src/components/features/FilterArea/FilterArea.jsx
@@ -29,21 +29,11 @@ export default function FilterArea({ onSubmit }) {
   const { globalLanguage } = useGlobalLanguage();
   const locale = localeMap[globalLanguage] || "en-US";
 
-  //const [selectTotalParticipants, setSelectTotalParticipants] = useState(null);
-  //const [country, setCountry] = useState([]);
-  //const [language, setLanguage] = useState([]);
-  //const [duration, setDuration] = useState("");
-  //const [dates, setDates] = useState(null);
   const [languages, setLanguages] = useState([]);
   const [countries, setCountries] = useState([]);
 
   const translateText = TranslateText({ globalLanguage });
 
-  /*  const options = [
-    { value: { min: 1, max: 5 }, label: "1 a 5" },
-    { value: { min: 6, max: 10 }, label: "6 a 10" },
-    { value: { min: 10, max: null }, label: "10+" },
-  ]; */
   const options = [
     { value: "1-5", label: "1 a 5" },
     { value: "6-10", label: "6 a 10" },
@@ -80,10 +70,6 @@ export default function FilterArea({ onSubmit }) {
       const videos = await getVideosByParameters(toFilter);
       onSubmit(toFilter);
       reset();
-      //setCountry([]);
-      //setLanguage([]);
-      //setDuration("");
-      //setSelectTotalParticipants(null);
     } catch (error) {
       console.error("Erro ao buscar vídeos filtrados:", error);
     }
@@ -99,11 +85,6 @@ export default function FilterArea({ onSubmit }) {
           render={({ field }) => (
             <StyledSelect
               {...field}
-              //defaultValue={selectTotalParticipants}
-              /* onChange={(e) => {
-                setSelectTotalParticipants(e.value);
-                field.onChange(e);
-              }} */
               isSearchable={false}
               placeholder={translateText.totalParticipantsPlaceholder}
               options={options}
@@ -121,11 +102,6 @@ export default function FilterArea({ onSubmit }) {
             <StyledSelect
               {...field}
               isMulti
-              // value={country}
-              /*    onChange={(selected) => {
-                setCountry(selected);
-                field.onChange(selected);
-              }} */
               isSearchable={true}
               placeholder={translateText.countryPlaceholder}
               options={countries.map((country) => ({
@@ -146,11 +122,6 @@ export default function FilterArea({ onSubmit }) {
             <StyledSelect
               {...field}
               isMulti
-              //value={language}
-              /*onChange={(selected) => {
-                setLanguage(selected);
-                field.onChange(selected);
-              }}*/
               isSearchable={true}
               placeholder={translateText.languagePlaceholder}
               options={languages.map((l) => ({
@@ -170,12 +141,6 @@ export default function FilterArea({ onSubmit }) {
             <StyledInput
               {...field}
               placeholder={translateText.durationPlaceholder}
-              /* {...register("duration")}
-              onChange={(e) => {
-                setDuration(e.target.value);
-                field.onChange(e.target.value);
-              }}
-              value={duration} */
               type="number"
             />
           )}
@@ -190,10 +155,6 @@ export default function FilterArea({ onSubmit }) {
           render={({ field }) => (
             <Calendar
               {...field}
-              /*onChange={(dates) => {
-                setDates(dates.value);
-                field.onChange(dates.value);
-              }} */
               appendTo="self"
               placeholder={translateText.calendarPlaceholder}
               readOnlyInput
